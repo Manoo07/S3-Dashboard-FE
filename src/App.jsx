82e48a2c3cafff385a1dad4d3bae548e927a9a80
Manoo07 <manoharboinapalli1196@gmail.com>
2024-11-03 17:06:08 +0530

    reverted to v1

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,16 @@ import Dashboard from './components/Dashboard';
 import Download from './components/Download';
 import Upload from './components/Upload';
 import Retrieve from './components/Retrieve';
-import ProtectedRoute from './components/ProtectedRoute';
-import LoginButton from './components/LoginButton';
 import Navbar from './components/Navbar';
 
 const App = () => (
   <Router>
     <Navbar />
     <Routes>
-      <Route path="/" element={<LoginButton />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/download" element={<ProtectedRoute><Download /></ProtectedRoute>} />
-      <Route path="/upload" element={<ProtectedRoute><Upload /></ProtectedRoute>} />
-      <Route path="/retrieve" element={<ProtectedRoute><Retrieve /></ProtectedRoute>} />
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/download" element={<Download />} />
+      <Route path="/upload" element={<Upload />} />
+      <Route path="/retrieve" element={<Retrieve />} />
     </Routes>
   </Router>
 );
